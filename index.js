de4a7c096570d1f04585e5620461f5c807b8d7bf
Jared Hanson <jaredhanson@gmail.com>
2016-09-27 21:40:15 -0700

    Update deps, tests.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-exports.hello = 'world';
+exports = module.exports = {
+  'response/idtoken': require('./xom/response/idtoken'),
+};
+
+exports.load = function(id) {
+  try {
+    return require('./xom/' + id);
+  } catch (ex) {
+    if (ex.code == 'MODULE_NOT_FOUND') { return; }
+    throw ex;
+  }
+};
--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,14 @@
-/* global describe, it */
+/* global describe, it, expect */
 
-var pkg = require('..');
 var expect = require('chai').expect;
+var source = require('..');
 
 
 describe('nodex-aaa-oauth2-openid', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export manifest', function() {
+    expect(source).to.be.an('object');
+    expect(source['response/idtoken']).to.be.a('function');
   });
   
 });
