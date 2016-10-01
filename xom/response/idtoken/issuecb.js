321f455af26f1cc23c57712b65ce23bbe0527cec
Jared Hanson <jaredhanson@gmail.com>
2016-09-30 19:57:12 -0700

    Add TODO items.

--- xom/response/idtoken/issuecb.js
+++ xom/response/idtoken/issuecb.js
@@ -1,6 +1,12 @@
 exports = module.exports = function(acs) {
   
   return function issueIDToken(client, user, ares, areq, bound, cb) {
+    // TODO: Include auth_time when max_age is sent
+    // TODO: Add nonce if in request
+    
+    // http://openid.net/specs/openid-connect-core-1_0.html, Section 2
+    
+    
     var bound = {
       client: client,
       redirectURI: redirectURI,
