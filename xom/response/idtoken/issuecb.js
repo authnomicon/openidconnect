3acdaef453e3bce161a11ce6c9e43ca643a73a02
Jared Hanson <jaredhanson@gmail.com>
2016-10-03 07:44:41 -0700

    Add TODO items.

--- xom/response/idtoken/issuecb.js
+++ xom/response/idtoken/issuecb.js
@@ -3,6 +3,8 @@ exports = module.exports = function(acs) {
   return function issueIDToken(client, user, ares, areq, bound, cb) {
     // TODO: Include auth_time when max_age is sent
     // TODO: Add nonce if in request
+    // TODO: `none` alg is allowable in `code` exchanges, if client selects it
+    //       when registering, secion 2
     
     // http://openid.net/specs/openid-connect-core-1_0.html, Section 2
     
