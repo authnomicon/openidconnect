528af9a10c0d5e05d77765a46a8b411ca89425ce
Jared Hanson <jaredhanson@gmail.com>
2017-03-28 12:18:37 -0700

    Add more tests.

--- test/package.test.js
+++ test/package.test.js
@@ -8,7 +8,16 @@ describe('@authnomicon/aaa-oauth2-openid', function() {
   
   it('should export manifest', function() {
     expect(pkg).to.be.an('object');
+    expect(Object.keys(pkg)).to.have.length(9);
+    
+    expect(pkg['ext/openid']).to.be.a('function');
     expect(pkg['implicit/idtoken']).to.be.a('function');
+    expect(pkg['implicit/idtokentoken']).to.be.a('function');
+    expect(pkg['hybrid/codeidtoken']).to.be.a('function');
+    expect(pkg['hybrid/codetoken']).to.be.a('function');
+    expect(pkg['hybrid/codeidtokentoken']).to.be.a('function');
+    expect(pkg['tokens/id/interpret']).to.be.a('function');
+    expect(pkg['tokens/id/translate']).to.be.a('function');
   });
   
 });
