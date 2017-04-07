6a7e7dcd970295164fedd705b7a05e0e1b093e6e
Jared Hanson <jaredhanson@gmail.com>
2017-04-07 16:26:40 -0700

    Cross-origin confirmation claims.

--- app/tokens/id/interpret.js
+++ app/tokens/id/interpret.js
@@ -10,11 +10,35 @@ exports = module.exports = function() {
     }
     */
     
-    
     var ctx = {};
     ctx.subject = { id: claims.sub };
     ctx.client = { id: claims.aud };
     ctx.csrfToken = claims.rfp;
+    
+    if (claims.cnf) {
+      ctx.confirmation = [];
+      
+      keys = Object.keys(claims.cnf);
+      for (i = 0, len = keys.length; i < len; ++i) {
+        key = keys[i];
+       
+        switch (key) {
+        case 'co_challenge':
+          ctx.confirmation.push({
+            method: 'cross-origin',
+            origin: claims.cnf.co_origin,
+            challenge: claims.cnf.co_challenge,
+            transform: claims.cnf.co_challenge_method || 'none'
+          });
+          break;
+        case 'co_origin':
+          break;
+        default:
+          ctx.confirmation.push({ method: 'unknown', name: key });
+          break;
+        }
+      }
+    }
     
     return cb(null, ctx);
   };
--- app/tokens/id/translate.js
+++ app/tokens/id/translate.js
@@ -1,7 +1,8 @@
 exports = module.exports = function() {
   
   return function translate(ctx, options, cb) {
-    var claims = {};
+    var claims = {}
+      , conf;
     
     if (ctx.user) {
       claims.sub = ctx.user.id;
@@ -13,6 +14,19 @@ exports = module.exports = function() {
     if (ctx.csrfToken) {
       // https://tools.ietf.org/html/draft-bradley-oauth-jwt-encoded-state-07
       claims.rfp = ctx.csrfToken;
+    }
+    
+    if (ctx.confirmation) {
+      conf = ctx.confirmation;
+      switch (conf.method) {
+      case 'cross-origin':
+        claims.cnf = {};
+        claims.cnf.co_origin = conf.origin;
+        claims.cnf.co_challenge = conf.challenge;
+        break;
+      default:
+        // TODO: throw error;
+      }
     }
     
     return cb(null, claims);
