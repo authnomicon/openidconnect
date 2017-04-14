1123fd0e7f2e46d6168161b97d2dddce9d3be614
Jared Hanson <jaredhanson@gmail.com>
2017-04-14 12:24:44 -0700

    Add EJS.

--- app/handlers/session/check.js
+++ app/handlers/session/check.js
@@ -1,13 +1,14 @@
 exports = module.exports = function() {
-  var path = require('path');
+  var path = require('path')
+    , ejs = require('ejs');
   
   
   function respond(req, res) {
-    //res.render('sessioncheck');
-    var f = path.join(__dirname, '../../../www/check.html');
-    console.log(f);
-    
-    res.sendFile(f);
+    var filename = path.join(__dirname, '../../../www/session/check.html.ejs');
+    ejs.renderFile(filename, res.locals, function(err, html) {
+      if (err) { return next(err); }
+      return res.send(html);
+    });
   }
   
   
