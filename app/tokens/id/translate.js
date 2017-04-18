0d97e5638e70f85849dc12c39e250485da618562
Jared Hanson <jaredhanson@gmail.com>
2017-04-18 10:43:47 -0700

    Remove confirmation claims.

--- app/tokens/id/translate.js
+++ app/tokens/id/translate.js
@@ -1,32 +1,13 @@
 exports = module.exports = function() {
   
   return function translate(ctx, options, cb) {
-    var claims = {}
-      , conf;
+    var claims = {};
     
     if (ctx.user) {
       claims.sub = ctx.user.id;
     }
     if (ctx.client) {
       claims.aud = ctx.client.id;
-    }
-    
-    if (ctx.csrfToken) {
-      // https://tools.ietf.org/html/draft-bradley-oauth-jwt-encoded-state-07
-      claims.rfp = ctx.csrfToken;
-    }
-    
-    if (ctx.confirmation) {
-      conf = ctx.confirmation;
-      switch (conf.method) {
-      case 'cross-origin':
-        claims.cnf = {};
-        claims.cnf.co_origin = conf.origin;
-        claims.cnf.co_challenge = conf.challenge;
-        break;
-      default:
-        // TODO: throw error;
-      }
     }
     
     return cb(null, claims);
