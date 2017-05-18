72a93bb5480628fbeaf8eb1ed2f17125ad1cca81
Jared Hanson <jaredhanson@gmail.com>
2017-05-18 15:48:37 -0700

    Move files.

--- app/http/handlers/cotc/verifier/iframe.js
+++ app/http/handlers/cotc/verifier/iframe.js
@@ -4,7 +4,7 @@ exports = module.exports = function(initialize, csrfProtection, Tokens) {
   
   
   function respond(req, res, next) {
-    var filename = path.join(__dirname, '../../../../www/cotc/iframe.html.ejs');
+    var filename = path.join(__dirname, '../../../../../www/cotc/verifier/iframe.html.ejs');
     ejs.renderFile(filename, res.locals, function(err, html) {
       if (err) { return next(err); }
       return res.send(html);
