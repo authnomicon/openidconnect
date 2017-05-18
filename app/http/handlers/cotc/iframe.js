786cf5ecc1f5718fa545a14e409f1c5ad340276e
Jared Hanson <jaredhanson@gmail.com>
2017-05-18 15:14:19 -0700

    Add COTC verification.

---
+++ app/http/handlers/cotc/iframe.js
@@ -0,0 +1,20 @@
+exports = module.exports = function(initialize, csrfProtection, Tokens) {
+  var path = require('path')
+    , ejs = require('ejs');
+  
+  
+  function respond(req, res, next) {
+    var filename = path.join(__dirname, '../../../../www/cotc/iframe.html.ejs');
+    ejs.renderFile(filename, res.locals, function(err, html) {
+      if (err) { return next(err); }
+      return res.send(html);
+    });
+  }
+  
+  
+  return [
+    respond
+  ];
+};
+
+exports['@require'] = [];
