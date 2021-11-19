17d427c72c4fd74f6493d645bff83014088e40c9
Jared Hanson <jaredhanson@gmail.com>
2021-11-19 11:51:28 -0800

    Construct claims for ID token.

--- com/oauth2/token/http/response/idtoken.js
+++ com/oauth2/token/http/response/idtoken.js
@@ -1,9 +1,21 @@
-exports = module.exports = function() {
+exports = module.exports = function(idts) {
   
   return function id_token(txn, cb) {
-    var jwt = 'TODO';
-    return cb(null, { id_token: jwt });
+    var msg = {};
+    msg.issuer = txn.issuer;
+    msg.user = txn.user;
+    msg.client = txn.client;
+    
+    // TODO: Add scope to the message, so user claims can be filtered
+    
+    idts.issue(msg, function(err, token) {
+      if (err) { return cb(err); }
+      return cb(null, { id_token: token });
+    });
   }
 };
 
 exports['@implements'] = 'http://i.authnomicon.org/oauth2/token/http/ResponseParameters';
+exports['@require'] = [
+  '../../../../sts/id',
+];
--- com/sts/id/default.js
+++ com/sts/id/default.js
@@ -1,15 +1,26 @@
-exports = module.exports = function() {
+exports = module.exports = function(jwt) {
   
   return {
     issue: function(msg, cb) {
-      console.log('TODO: issue ID token...');
-      console.log(msg);
-    
-      return cb(null, 'TODO-id_token');
+      var claims = {
+        iss: msg.issuer,
+        sub: msg.user.id,
+        aud: msg.client.id
+      };
+      if (msg.expires) { claims.exp = Math.floor(msg.expires.valueOf() / 1000); }
+      else { claims.exp = Math.floor(Date.now() / 1000) + 7200; } // 2 hours from now
+      if (msg.issued) { claims.iat = Math.floor(msg.issued.valueOf() / 1000); }
+      else { claims.iat = Math.floor(Date.now() / 1000); }
+      
+      jwt.sign(claims, function(err, token) {
+        if (err) { return cb(err); }
+        return cb(null, token);
+      });
     }
   }
 };
 
 exports['@singleton'] = true;
 exports['@require'] = [
+  'http://i.bixbyjs.org/jose/jwt'
 ];
