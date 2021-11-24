55cdda53a172e44389fb7dc6ff8d470bbaea5b31
Jared Hanson <jaredhanson@gmail.com>
2021-11-24 07:40:10 -0800

    Add more message parameters to ID token service.

--- com/oauth2/token/http/response/idtoken.js
+++ com/oauth2/token/http/response/idtoken.js
@@ -2,9 +2,11 @@ exports = module.exports = function(idts) {
   
   return function id_token(txn, cb) {
     var msg = {};
-    msg.issuer = txn.issuer;
+    if (txn.issuer) { msg.issuer = txn.issuer; }
     msg.user = txn.user;
     msg.client = txn.client;
+    if (txn.scope) { msg.scope = txn.scope; }
+    if (txn.authContext) { msg.authContext = txn.authContext; }
     
     // TODO: Add scope to the message, so user claims can be filtered
     
