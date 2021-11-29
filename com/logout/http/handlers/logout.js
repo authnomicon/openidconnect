8eb47f408b8dce607f073f927f88c9218b77651f
Jared Hanson <jaredhanson@gmail.com>
2021-11-28 21:29:28 -0800

    Verify ID token and logout.

--- com/logout/http/handlers/logout.js
+++ com/logout/http/handlers/logout.js
@@ -1,14 +1,40 @@
-exports = module.exports = function() {
-  return [
-    function(req, res, next) {
-      console.log('!!! LOGOUT !!!');
-      console.log(req.query);
-      console.log(req.session);
+exports = module.exports = function(idts, authenticate) {
+  
+  function verifyIDToken(req, res, next) {
+    var idTokenHint = req.query.id_token_hint;
+    
+    idts.verify(idTokenHint, function(err, msg) {
+      if (err) { return next(err); }
+      res.locals.idToken = msg;
+      next();
+    });
+  }
+  
+  function logout(req, res, next) {
+    var idToken = res.locals.idToken;
+    
+    if (idToken.authContext.sessionID === req.sessionID) {
+      req.logout();
+      res.redirect('/')
       
-      
+      // TODO: Async logout in passport
+      /*
+      return req.logout(function() {
+        console.log('LOGGED OUT!');
+        res.redirect('/');
+      })
+      */
     }
+  }
+  
+  return [
+    authenticate('anonymous'),
+    verifyIDToken,
+    logout
   ];
 };
 
 exports['@require'] = [
+  '../../../sts/id',
+  'http://i.bixbyjs.org/http/middleware/authenticate'
 ];
