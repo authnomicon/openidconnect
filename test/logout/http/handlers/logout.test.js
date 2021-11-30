f09d6cfb8218704b0a91b4a04fe90894025a1f52
Jared Hanson <jaredhanson@gmail.com>
2021-11-30 07:56:43 -0800

    Add tests.

--- test/logout/http/handlers/logout.test.js
+++ test/logout/http/handlers/logout.test.js
@@ -13,4 +13,38 @@ describe('logout/http/handlers/logout', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
+  
+  function authenticate() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function state() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function session() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  it('should create handler', function() {
+    var idTokenService = new Object();
+    var authenticateSpy = sinon.spy(authenticate);
+    var stateSpy = sinon.spy(state);
+    var sessionSpy = sinon.spy(session);
+    
+    var handler = factory(idTokenService, authenticateSpy, stateSpy, sessionSpy);
+    
+    expect(sessionSpy).to.be.calledOnce;
+    expect(stateSpy).to.be.calledOnce;
+    expect(stateSpy).to.be.calledAfter(sessionSpy);
+    expect(authenticateSpy).to.be.calledOnce;
+    expect(authenticateSpy).to.be.calledAfter(stateSpy);
+  });
+  
 });
