bb875c981df6b917437596fcda222ef29d1ad93a
Jared Hanson <jaredhanson@gmail.com>
2021-12-02 13:03:30 -0800

    Add email claim to userinfo response.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
 - Added `email` claim to ID token.
+- Added `email` claim to UserInfo response.
 
 ## [0.0.2] - 2021-12-02
 ### Added
--- com/userinfo/http/handlers/userinfo.js
+++ com/userinfo/http/handlers/userinfo.js
@@ -17,6 +17,9 @@ exports = module.exports = function(authenticate, users) {
           if (user.name.givenName) { claims.given_name = user.name.givenName; }
           if (user.name.middleName) { claims.middle_name = user.name.middleName; }
         }
+        if (user.emails && user.emails.length) {
+          if (user.emails[0].value) { claims.email = user.emails[0].value; }
+        }
         
         res.json(claims);
       });
